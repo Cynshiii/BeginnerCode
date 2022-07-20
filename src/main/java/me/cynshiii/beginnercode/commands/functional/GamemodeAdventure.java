fb9c4dbaf49f23c4fb0d75492f423fab39d64f69
Cynshiii <cynthiaemleigh@gmail.com>
2022-07-20 05:11:18 -0500

    add gamemode adventure

---
+++ src/main/java/me/cynshiii/beginnercode/commands/functional/GamemodeAdventure.java
@@ -0,0 +1,39 @@
+package me.cynshiii.beginnercode.commands.functional;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class GamemodeAdventure implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+
+		if (sender instanceof Player player){
+			if (player.hasPermission("BeginnerCode.Commands.Gamemode.Adventure")){
+				if (args.length == 0){
+					player.setGameMode(GameMode.ADVENTURE);
+					player.sendMessage(ChatColor.LIGHT_PURPLE + "Set gamemode to Adventure");
+				} else{
+					String playerName = args[0];
+					Player target = Bukkit.getServer().getPlayerExact(playerName);
+					if (target == null){
+						player.sendMessage(ChatColor.DARK_PURPLE + "That player is not online");
+					} else{
+						target.setGameMode(GameMode.ADVENTURE);
+						target.sendMessage(ChatColor.LIGHT_PURPLE + "Set gamemode to Adventure");
+					}
+				}
+			} else {
+				player.sendMessage(ChatColor.DARK_PURPLE + "You do not have access to this command");
+			}
+		}
+
+		return true;
+	}
+}
