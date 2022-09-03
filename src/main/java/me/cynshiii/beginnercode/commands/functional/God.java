76756be5218154a088026d4aff964d8a1adb8dab
Cynshiii <cynthiaemleigh@gmail.com>
2022-09-03 11:49:36 -0500

    remake commands froms cratch, reformat them and make them look nicer

--- src/main/java/me/cynshiii/beginnercode/commands/functional/God.java
+++ src/main/java/me/cynshiii/beginnercode/commands/functional/God.java
@@ -13,52 +13,35 @@ public class God implements CommandExecutor {
 	@Override
 	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
 
-		if (sender instanceof Player player){
-			if (player.hasPermission("beginnercode.commands.godCommand")){
-				//setting cmd permissions
-				if (args.length == 0){
-					//if player only gives command (re: /god)
-
-					if (player.isInvulnerable()){
-						//checking to see if theyre already invulnerable
-						player.setInvulnerable(false);
-						player.sendMessage(ChatColor.LIGHT_PURPLE + "Godmode disabled");
-						//if invulnerable, set to false and send message above
-					} else{
-						player.setInvulnerable(true);
-						player.sendMessage(ChatColor.LIGHT_PURPLE + "Godmode enabled");
-						//if not invulnerable, set to true and send msg above
-					}
-
-				} else{
-					String playerName = args[0];
-					Player target = Bukkit.getServer().getPlayerExact(playerName);
-					/*
-					setting two variables- playerName being the first argument a player puts in the command
-					re: /god [argument]
-					this variable would take [argument]
-					also setting target variable to be the variable we set just before that, for easy use below
-					 */
-					if (target == null){
-						//if target is offline, send message below to player
-						player.sendMessage(ChatColor.DARK_PURPLE + "That player is not online");
-					} else if (target.isInvulnerable()){
-						//checking invulnerability status of target
-						target.setInvulnerable(false);
-						target.sendMessage(ChatColor.LIGHT_PURPLE + "Godmode disabled");
-						// if target is invulnerable, set false and send msg above
-					} else{
-						target.setInvulnerable(true);
-						target.sendMessage(ChatColor.LIGHT_PURPLE + "Godmode enabled");
-						// if target is not invulnerable, set to true and send msg above
-					}
-				}
-			} else{
-				player.sendMessage(ChatColor.DARK_PURPLE + "You do not have access to that command");
-				// if player has no access to command, send message above
-			}
+		if (!(sender instanceof Player player)) {
+			return true;
+		}
+		if (!(player.hasPermission("beginnercode.commands.god"))) {
+			player.sendMessage(ChatColor.DARK_PURPLE + "You do not have access to this command");
 		}
 
+		if (args.length == 0) {
+			if (player.isInvulnerable()) {
+				player.setInvulnerable(false);
+				player.sendMessage(ChatColor.LIGHT_PURPLE + "God disabled");
+			} else {
+				player.setInvulnerable(true);
+				player.sendMessage(ChatColor.LIGHT_PURPLE + "God enabled");
+			}
+		} else {
+			String playerName = args[0];
+			Player target = Bukkit.getServer().getPlayerExact(playerName);
+
+			if (target == null) {
+				player.sendMessage(ChatColor.DARK_PURPLE + "That player is not online");
+			} else if (target.isInvulnerable()) {
+				target.setInvulnerable(false);
+				target.sendMessage(ChatColor.LIGHT_PURPLE + "God disabled");
+			} else {
+				target.setInvulnerable(true);
+				target.sendMessage(ChatColor.LIGHT_PURPLE + "God enabled");
+			}
+		}
 		return true;
 	}
 }
--- src/main/java/me/cynshiii/beginnercode/commands/functional/ItemName.java
+++ src/main/java/me/cynshiii/beginnercode/commands/functional/ItemName.java
@@ -14,35 +14,26 @@ public class ItemName implements CommandExecutor {
 	@Override
 	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
 
-		if (sender instanceof Player player){
-			if(player.hasPermission("beginnercode.commands.itemname")){
-				// setting command permission
-				if (args.length == 0){
-					player.sendMessage(ChatColor.DARK_PURPLE + "No arguments given");
-					// if player just runs the command (re: /itemname), send message above
-				} else{
-					ItemStack inhand = player.getItemInHand();
-					//setting variable "inhand" to be the item in player's hand at the time of running command
-					if (inhand != null){
-						ItemMeta itemMeta = inhand.getItemMeta();
-						//setting itemMeta variable to catch inhand item meta
 
-						itemMeta.setDisplayName(args[0].replace("_", " "));
-						//setting the itemmeta of inhand to given arguments, replacing any '_' with spaces
-						inhand.setItemMeta(itemMeta);
-						// setting itemmeta to current so it saves
-
-						String itemName = player.getItemInHand().getItemMeta().getDisplayName();
-						//setting itemName variable to use in message below
-
-						player.sendMessage(ChatColor.LIGHT_PURPLE + "Your item has been renamed to " + itemName);
-						//sending player message with new item name using itemName variable
-
-					}
-				}
-			}
+		if (!(sender instanceof Player player)){
+			return true;
+		}
+		if (!(player.hasPermission("beginnercode.commands.itemname"))){
+			player.sendMessage(ChatColor.DARK_PURPLE + "You do not have access to this command");
 		}
 
+		if (args.length == 0){
+			player.sendMessage(ChatColor.DARK_PURPLE + "No arguments given");
+		} else{
+			ItemStack inhand = player.getItemInHand();
+			if (inhand != null){
+				ItemMeta itemMeta = inhand.getItemMeta();
+				itemMeta.setDisplayName(args[0].replace("_", " "));
+				inhand.setItemMeta(itemMeta);
+				String itemName = player.getItemInHand().getItemMeta().getDisplayName();
+				player.sendMessage(ChatColor.LIGHT_PURPLE + "Your item has been renamed to " + itemName);
+			}
+		}
 		return true;
 	}
 }
