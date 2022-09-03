0b7ad4d69b2f011012832e3155d3eb0c648513a9
Cynshiii <cynthiaemleigh@gmail.com>
2022-09-03 01:03:41 -0500

    make code prettier and easier to read

--- src/main/java/me/cynshiii/beginnercode/commands/functional/ClearInventory.java
+++ src/main/java/me/cynshiii/beginnercode/commands/functional/ClearInventory.java
@@ -12,19 +12,16 @@ public class ClearInventory implements CommandExecutor {
 	@Override
 	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
 
-		if (sender instanceof Player player){
-			if (player.hasPermission("beginnercode.commands.clear")){
-				// setting permission node
-				player.getInventory().clear();
-				//getting the players inventory and clearing it
-				player.sendMessage(ChatColor.LIGHT_PURPLE + "Inventory Cleared");
-				//sending the player a message about clearing inv
-			} else{
-				player.sendMessage(ChatColor.DARK_PURPLE + "You do not have access to that command");
-				//sending player no access message if they dont have the above permission node
-			}
+		if(!(sender instanceof Player player))
+			return true;
+		if(!(player.hasPermission("beginnercode.commands.clearinv"))){
+			player.sendMessage(ChatColor.DARK_PURPLE + "You do not have access to this command");
+			return true;
 		}
+			player.getInventory().clear();
+			player.sendMessage(ChatColor.LIGHT_PURPLE + "Inventory Cleared");
 
-		return true;
+			return true;
+		}
 	}
-}
+
