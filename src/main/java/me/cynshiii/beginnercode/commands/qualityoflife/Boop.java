d1d5da887ec533811c8055ad2a38d6c093615ea3
Cynshiii <cynthiaemleigh@gmail.com>
2022-09-06 21:54:24 -0500

    rewrite code, add anon option, add error nets

--- src/main/java/me/cynshiii/beginnercode/commands/qualityoflife/Boop.java
+++ src/main/java/me/cynshiii/beginnercode/commands/qualityoflife/Boop.java
@@ -1,5 +1,6 @@
 package me.cynshiii.beginnercode.commands.qualityoflife;
 
+import net.kyori.adventure.sound.Sound;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -8,29 +9,49 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+
 public class Boop implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
 
-		if (!(sender instanceof Player player)){
+		if (!(sender instanceof Player player))
 			return true;
-		}
+
 		if (!(player.hasPermission("beginnercode.commands.boop"))){
 			player.sendMessage(ChatColor.DARK_PURPLE + "You do not have access to this command");
 			return true;
 		}
 
-		String playerName = args[0];
-		Player target = Bukkit.getServer().getPlayerExact(playerName);
+		String targetName = args[0];
+		Player target = Bukkit.getServer().getPlayerExact(targetName);
 
 		if (target == null){
 			player.sendMessage(ChatColor.DARK_PURPLE + "That player is not online");
 			return true;
 		}
-		target.sendMessage(ChatColor.LIGHT_PURPLE + "Someone has booped you!");
-		player.sendMessage(ChatColor.LIGHT_PURPLE + "You have booped " + playerName + "!");
 
+		if (args.length > 2){
+			player.sendMessage(ChatColor.DARK_PURPLE + "Error: Too many arguments");
+			return true;
+		}
+		if (args.length == 2){
+			if (!(args[1].equals("-a"))){
+				player.sendMessage(ChatColor.DARK_PURPLE + "Error: Unexpected arguments");
+				return true;
+			} else{
+				target.sendMessage( ChatColor.LIGHT_PURPLE + "Someone has anonymously booped you!");
+				target.playSound(target.getLocation(), org.bukkit.Sound.BLOCK_NOTE_BLOCK_XYLOPHONE, 0.5F, 1F);
+				player.sendMessage(ChatColor.LIGHT_PURPLE + "You have booped " + targetName + "!");
+			}
+		} else if (args.length == 1){
+			target.sendMessage(ChatColor.DARK_PURPLE + player.getName() + ChatColor.LIGHT_PURPLE + " has booped you!");
+			target.playSound(target.getLocation(), org.bukkit.Sound.BLOCK_NOTE_BLOCK_XYLOPHONE, 0.5F, 1F);
+			player.sendMessage(ChatColor.LIGHT_PURPLE + "You have booped " + targetName + "!");
+		} else{
+			player.sendMessage(ChatColor.DARK_PURPLE + "You cannot boop yourself!");
+		}
 		return true;
 	}
 }
