0a1ee250f87a9d668aa63e961f4d9365dd29d12f
Cynshiii <cynthiaemleigh@gmail.com>
2022-09-14 16:54:31 -0500

    rewrite gamemodes

--- src/main/java/me/cynshiii/beginnercode/commands/functional/gamemodes/Adventure.java
+++ src/main/java/me/cynshiii/beginnercode/commands/functional/gamemodes/Adventure.java
@@ -14,39 +14,28 @@ public class Adventure implements CommandExecutor {
 	@Override
 	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
 
-		if (sender instanceof Player player){
-			if (player.hasPermission("BeginnerCode.Commands.Gamemode.Adventure")){
-				// setting permission node
-				if (args.length == 0){
-					// if player provides no arguments (re: just does /gamemode adventure
-					player.setGameMode(GameMode.ADVENTURE);
-					player.sendMessage(ChatColor.LIGHT_PURPLE + "Set gamemode to Adventure");
-					// set gamemode to adventure and send message above
-				} else{
-					//if player does provide argument (re: does /gamemode aventure [player name]
-					String playerName = args[0];
-					Player target = Bukkit.getServer().getPlayerExact(playerName);
-					/*
-					setting two variables- playerName being the first argument a player puts in the command
-					re: /gamemode adventure [argument]
-					this variable would take [argument]
-					also setting target variable to be the variable we set just before that, for easy use below
-					 */
-					if (target == null){
-						// if target is offline send message below
-						player.sendMessage(ChatColor.DARK_PURPLE + "That player is not online");
-					} else{
-						// if target is online, set gamemode to adventure and send message below
-						target.setGameMode(GameMode.ADVENTURE);
-						target.sendMessage(ChatColor.LIGHT_PURPLE + "Set gamemode to Adventure");
-					}
-				}
-			} else {
-				player.sendMessage(ChatColor.DARK_PURPLE + "You do not have access to this command");
-				// send msg above if player sending cmd does not have access
-			}
+		if (!(sender instanceof Player player))
+			return true;
+
+		if (!(player.hasPermission("beginnercode.commands.gamemode.adventure"))){
+			player.sendMessage(ChatColor.DARK_PURPLE + "You do not have access to this command");
+			return true;
 		}
 
+		if (args.length == 0){
+			player.setGameMode(GameMode.ADVENTURE);
+			player.sendMessage(ChatColor.LIGHT_PURPLE + "Set gamemode to Adventure");
+		} else{
+			String playerName = args[0];
+			Player target = Bukkit.getServer().getPlayerExact(playerName);
+			if (!(target == null)) {
+				target.setGameMode(GameMode.ADVENTURE);
+				target.sendMessage(ChatColor.LIGHT_PURPLE + "Set gamemode to Adventure");
+			} else{
+				player.sendMessage(ChatColor.DARK_PURPLE + "That player is not online");
+				return true;
+			}
+		}
 		return true;
 	}
 }
--- src/main/java/me/cynshiii/beginnercode/commands/functional/gamemodes/Spectator.java
+++ src/main/java/me/cynshiii/beginnercode/commands/functional/gamemodes/Spectator.java
@@ -14,37 +14,28 @@ public class Spectator implements CommandExecutor {
 	@Override
 	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
 
-		if (sender instanceof Player player){
-			if (player.hasPermission("BeginnerCode.Commands.Gamemode.Spectator")){
-				// setting command permissions
-				if (args.length == 0){
-					// if player provides no arguments (re: /gamemode spectator) set gamemode to spectator and send msg below)
-					player.setGameMode(GameMode.SPECTATOR);
-					player.sendMessage(ChatColor.LIGHT_PURPLE + "Set gamemode to Spectator");
-				} else{
-					String playerName = args[0];
-					Player target = Bukkit.getServer().getPlayerExact(playerName);
-					/*
-					setting two variables- playerName being the first argument a player puts in the command
-					re: /gamemode spectator [argument]
-					this variable would take [argument]
-					also setting target variable to be the variable we set just before that, for easy use below
-					 */
-					if (target == null){
-						// if target is not online send message below to player
-						player.sendMessage(ChatColor.DARK_PURPLE + "That player is not online");
-					} else{
-						// if target is online, set gamemode to spectator and send msg below
-						target.setGameMode(GameMode.SPECTATOR);
-						target.sendMessage(ChatColor.LIGHT_PURPLE + "Set gamemode to Spectator");
-					}
-				}
-			} else {
-				player.sendMessage(ChatColor.DARK_PURPLE + "You do not have access to this command");
-				//send to player if they have no permission for the command
-			}
+		if (!(sender instanceof Player player))
+			return true;
+
+		if (!(player.hasPermission("beginnercode.commands.gamemode.spectator"))){
+			player.sendMessage(ChatColor.DARK_PURPLE + "You do not have access to this command");
+			return true;
 		}
 
+		if (args.length == 0){
+			player.setGameMode(GameMode.SPECTATOR);
+			player.sendMessage(ChatColor.LIGHT_PURPLE + "Set gamemode to Spectator");
+		} else{
+			String playerName = args[0];
+			Player target = Bukkit.getServer().getPlayerExact(playerName);
+			if (!(target == null)) {
+				target.setGameMode(GameMode.SPECTATOR);
+				target.sendMessage(ChatColor.LIGHT_PURPLE + "Set gamemode to Spectator");
+			} else{
+				player.sendMessage(ChatColor.DARK_PURPLE + "That player is not online");
+				return true;
+			}
+		}
 		return true;
 	}
 }
